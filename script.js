2b4ec87964f7d7b288c443a77af97d8850eb8227
Josephine Simar <sijo20be@student.ju.se>
2020-09-29 12:58:16 +0200

    Informationsvy om varje stad med ruta att bocka i

--- script.js
+++ script.js
@@ -15,35 +15,31 @@ fetch('land.json')
             })
             .then(function(staddata){
                 console.log("staddata: ", staddata);
-
                 printMenu(landdata,staddata);
-                
-                
                 
             })
         })
         
-        
-        //funktion som skriver ut länderna till en menyrad i head-diven och gör dem klickbara
-        function printMenu(landdata,staddata){
-            console.log("menyn skrivs ut");
-            for(let i=0; i<landdata.length; i++){
-                let land = document.createElement('li');
-                menu.appendChild(land);
-                land.insertAdjacentHTML('beforeend', landdata[i].countryname);
-                land.addEventListener('click', function(){
-                    printCities(landdata[i],staddata);
-                });
-            }
-            
-            
 
+//funktion som skriver ut länderna till en menyrad i head-diven och gör dem klickbara
+function printMenu(landdata,staddata){
+    console.log("menyn skrivs ut");
+    for(let i=0; i<landdata.length; i++){
+        let land = document.createElement('li');
+        menu.appendChild(land);
+        land.insertAdjacentHTML('beforeend', landdata[i].countryname);
+        land.addEventListener('click', function(){
+            printCities(landdata[i],staddata);
+        });
+    }
 }
 
 //funktion som anropas vid klick på landsnamn, skriver ut landets städer i content-diven
 //byter också ut rubriken till klicka på en STAD
 function printCities(landdata, staddata){
     document.getElementById('cityList').innerHTML='';
+    document.getElementById('fact').innerHTML='';
+    document.getElementById('headline').innerHTML='Klicka på en stad för att se läsa om den';
     console.log("städer skrivs ut för land: ", landdata.countryname);
     for(let j=0; j<staddata.length; j++){
         if(staddata[j].countryid === landdata.id){
@@ -51,18 +47,21 @@ function printCities(landdata, staddata){
             cityList.appendChild(city);
             city.insertAdjacentHTML('beforeend', staddata[j].stadname);
             city.addEventListener('click', function(){
-                 printInfo(staddata[j]);
+                 printInfo(staddata[j], landdata);
                  console.log("klick på staden");
             });
         }
-        
     }
 }
 
 //funktion som anropas vid klick på stadsnamn, skriver ut info i infoContent-diven
 //har även en ruta som kan klickas i
-function printInfo(staddata){
-    console.log("skriver ut info om: ", staddata.stadname)
+function printInfo(staddata, landdata){
+    console.log("skriver ut info om: ", staddata.stadname);
+    document.getElementById('fact').innerHTML=staddata.stadname + ' är en stad i ' + landdata.countryname + ' där det bor ' + staddata.population + ' personer.' + '<br/>' + 'Jag har besökt ' + staddata.stadname;
+    let visitedBtn = document.createElement('INPUT');
+    visitedBtn.setAttribute('type', 'checkbox');
+    fact.appendChild(visitedBtn);
 
 
 }
